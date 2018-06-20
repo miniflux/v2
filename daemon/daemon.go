261695c14c2d768f392cfb774e7940660edaa3d9
Frédéric Guillot <fred@miniflux.net>
2018-06-19 20:55:29 -0700

    Improve memory usage debug log

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -32,8 +32,9 @@ func Run(cfg *config.Config, store *storage.Storage) {
 		for {
 			var m runtime.MemStats
 			runtime.ReadMemStats(&m)
-			logger.Debug("Alloc=%vK, TotalAlloc=%vK, Sys=%vK, NumGC=%v, GoRoutines=%d, NumCPU=%d",
-				m.Alloc/1024, m.TotalAlloc/1024, m.Sys/1024, m.NumGC, runtime.NumGoroutine(), runtime.NumCPU())
+			logger.Debug("Sys=%vK, InUse=%vK, HeapInUse=%vK, StackSys=%vK, StackInUse=%vK, GoRoutines=%d, NumCPU=%d",
+				m.Sys/1024, (m.Sys-m.HeapReleased)/1024, m.HeapInuse/1024, m.StackSys/1024, m.StackInuse/1024,
+				runtime.NumGoroutine(), runtime.NumCPU())
 			time.Sleep(30 * time.Second)
 		}
 	}()
