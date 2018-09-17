8867b79fa551685442e5ad256515d914c9c034df
L11R <savely@krasovsky.me>
2018-09-17 20:28:11 +0300

    Unicode complaint truncate function for templates

--- template/common.go
+++ template/common.go
@@ -65,7 +65,7 @@ var templateCommonMap = map[string]string{
                 title="{{ t "Change entry status" }}"
                 data-toggle-status="true"
                 data-label-read="✔&#xfe0e;&nbsp;{{ t "Read" }}"
-                data-label-unread="✘&nbsp;{{ t "Unread" }}"
+                data-label-unread="✘&nbsp;{{ t "Mark as Unread" }}"
                 data-value="{{ if eq .entry.Status "read" }}read{{ else }}unread{{ end }}"
                 >{{ if eq .entry.Status "read" }}✘&nbsp;{{ t "Mark as Unread" }}{{ else }}✔&#xfe0e;&nbsp;{{ t "Read" }}{{ end }}</a>
         </li>
@@ -242,7 +242,7 @@ var templateCommonMap = map[string]string{
 
 var templateCommonMapChecksums = map[string]string{
 	"entry_pagination": "756ef122f3ebc73754b5fc4304bf05e59da0ab4af030b2509ff4c9b4a74096ce",
-	"item_meta":        "7c11a806a7db1890e7c5b348218991dcb82fb32fbe03604c333f2c2aec10aafa",
+	"item_meta":        "72a85584d546518f25978cc248567c2b955d5d18dfe3fc317db875141fdfc529",
 	"layout":           "2491695e33a496c9bd902a2cb5bc3a6a540f98ac7c24591d503a77ba0f5f0ebe",
 	"pagination":       "b592d58ea9d6abf2dc0b158621404cbfaeea5413b1c8b8b9818725963096b196",
 }
--- template/views.go
+++ template/views.go
@@ -560,9 +560,9 @@ var templateViewsMap = map[string]string{
                         title="{{ t "Change entry status" }}"
                         data-toggle-status="true"
                         data-label-read="✔&#xfe0e;&nbsp;{{ t "Read" }}"
-                        data-label-unread="✘&nbsp;{{ t "Unread" }}"
+                        data-label-unread="✘&nbsp;{{ t "Mark as Unread" }}"
                         data-value="{{ if eq .entry.Status "read" }}read{{ else }}unread{{ end }}"
-                        >{{ if eq .entry.Status "read" }}✘&nbsp;{{ t "Unread" }}{{ else }}✔&#xfe0e;&nbsp;{{ t "Read" }}{{ end }}</a>
+                        >{{ if eq .entry.Status "read" }}✘&nbsp;{{ t "Mark as Unread" }}{{ else }}✔&#xfe0e;&nbsp;{{ t "Read" }}{{ end }}</a>
                 </li>
                 <li>
                     <a href="#"
@@ -1369,7 +1369,7 @@ var templateViewsMapChecksums = map[string]string{
 	"edit_category":       "cee720faadcec58289b707ad30af623d2ee66c1ce23a732965463250d7ff41c5",
 	"edit_feed":           "1a8e342e4fac80e8b9c73537c7fe8aaf7f9e3e7af22f411927010897dd37e9c3",
 	"edit_user":           "9fe16d508bd648f791898ed5905335524fdf5a7633553f64c5bc2218652821b9",
-	"entry":               "82a0a4e715da94b12370b380072f1175c9f0e07b37e7f54a9adca4ed1fe015c0",
+	"entry":               "efc7f889811415e6693cc5aa8193ce983871f88b6dbd4d85298407000a2a9b79",
 	"feed_entries":        "bebc42317ca9e908fcdb98cc1c4a2dc3f4bb7ef6d4c288d3d3fba8f8339403b6",
 	"feeds":               "1006698abfe0962b656f27794bc44568515392da72b6fac0c03316de06024237",
 	"history_entries":     "f94e15d37d7604500cede7b583e03bf79c06be81c6597a4a43693f5712af2e13",
