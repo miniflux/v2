11dfcdd3d602f1a5e9a2cc87e0e296c6c5507ee8
Frédéric Guillot <fred@miniflux.net>
2018-10-08 15:50:15 -0700

    Fix typo in license header

--- http/response/json/json.go
+++ http/response/json/json.go
@@ -1,6 +1,6 @@
 // Copyright 2018 Frédéric Guillot. All rights reserved.
-// Use of this source code is governed by the MIT license
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
 
 package json // import "miniflux.app/http/response/json"
 
