e50e54d59c5aabb86c9aa1afde28015d444003bd
Gauthier Folzan <gauthier@wuhu.eu>
2019-03-10 01:43:27 +0100

    Touch actions for next/previous article

--- ui/static/js/touch_handler.js
+++ ui/static/js/touch_handler.js
@@ -1,6 +1,7 @@
 class TouchHandler {
     constructor() {
         this.reset();
+   this.toaster=document.getElementById("toaster");
     }
 
     reset() {
@@ -25,11 +26,21 @@ class TouchHandler {
     }
 
     findElement(element) {
+        if (element.classList.contains("touch-article")) {
+            return element;
+        }
         if (element.classList.contains("touch-item")) {
             return element;
         }
-
+       let parentElement = DomHelper.findParent(element, "touch-article");
+       if( parentElement == null) 
+       {
         return DomHelper.findParent(element, "touch-item");
+       }
+       else
+       { 
+          return parentElement;
+       }
     }
 
     onTouchStart(event) {
@@ -43,7 +54,7 @@ class TouchHandler {
         this.touch.element = this.findElement(event.touches[0].target);
     }
 
-    onTouchMove(event) {
+    onItemTouchMove(event) {
         if (event.touches === undefined || event.touches.length !== 1 || this.element === null) {
             return;
         }
@@ -64,8 +75,7 @@ class TouchHandler {
             event.preventDefault();
         }
     }
-
-    onTouchEnd(event) {
+    onItemTouchEnd(event) {
         if (event.touches === undefined) {
             return;
         }
@@ -83,14 +93,81 @@ class TouchHandler {
         this.reset();
     }
 
+    onArticleTouchMove(event) {
+        if (event.touches === undefined || event.touches.length !== 1 || this.element === null) {
+            return;
+        }
+
+        this.touch.move.x = event.touches[0].clientX;
+        this.touch.move.y = event.touches[0].clientY;
+
+        let distance = this.calculateDistance();
+        if (distance > 0) {
+            this.toaster.innerHTML = "Previous";
+   }else{
+            this.toaster.innerHTML = "Next";
+   }
+        let absDistance = Math.abs(distance);
+
+        if (absDistance > 0) {
+//            let opacity = 1 - (absDistance > 125 ? 0.9 : absDistance / 125 * 0.9);
+//            this.touch.element.style.opacity = opacity;
+       let tx = 40 - (absDistance > 90 ? 40 : absDistance / 90 * 40);
+       this.toaster.style.transform = "translate(-50%," + tx + "px)";
+            event.preventDefault();
+        }
+        if (absDistance > 100) {
+            this.toaster.style.opacity = 1.0;
+        } else {
+            this.toaster.style.opacity = 0.3;
+        }
+    }
+    onArticleTouchEnd(event) {
+        if (event.touches === undefined) {
+            return;
+        }
+
+        if (this.touch.element !== null) {
+            let distance = this.calculateDistance();
+            if (Math.abs(distance) > 150) {
+          if (distance < 100) {
+             let element = document.querySelector("a[data-page=next]");
+             if (element) {
+                document.location.href = element.href;
+             }
+          }
+          if (distance > 100) {
+             let element = document.querySelector("a[data-page=previous]");
+
+             if (element) {
+                document.location.href = element.href;
+             } 
+          }
+       }
+//            this.touch.element.style.opacity = 1;
+//            this.touch.element.style.transform = "none";
+            this.toaster.style.opacity = 0.3;
+       this.toaster.style.transform = "translate(-50%,40px)";
+            this.toaster.innerHTML = "";
+        }
+        this.reset();
+    }
+
     listen() {
-        let elements = document.querySelectorAll(".touch-item");
+        let itemElements = document.querySelectorAll(".touch-item");
+        let articleElements = document.querySelectorAll(".touch-article");
         let hasPassiveOption = DomHelper.hasPassiveEventListenerOption();
 
-        elements.forEach((element) => {
+        itemElements.forEach((element) => {
             element.addEventListener("touchstart", (e) => this.onTouchStart(e), hasPassiveOption ? { passive: true } : false);
-            element.addEventListener("touchmove", (e) => this.onTouchMove(e), hasPassiveOption ? { passive: false } : false);
-            element.addEventListener("touchend", (e) => this.onTouchEnd(e), hasPassiveOption ? { passive: true } : false);
+            element.addEventListener("touchmove", (e) => this.onItemTouchMove(e), hasPassiveOption ? { passive: false } : false);
+            element.addEventListener("touchend", (e) => this.onItemTouchEnd(e), hasPassiveOption ? { passive: true } : false);
+            element.addEventListener("touchcancel", () => this.reset(), hasPassiveOption ? { passive: true } : false);
+        });
+        articleElements.forEach((element) => {
+            element.addEventListener("touchstart", (e) => this.onTouchStart(e), hasPassiveOption ? { passive: true } : false);
+            element.addEventListener("touchmove", (e) => this.onArticleTouchMove(e), hasPassiveOption ? { passive: false } : false);
+            element.addEventListener("touchend", (e) => this.onArticleTouchEnd(e), hasPassiveOption ? { passive: true } : false);
             element.addEventListener("touchcancel", () => this.reset(), hasPassiveOption ? { passive: true } : false);
         });
     }
