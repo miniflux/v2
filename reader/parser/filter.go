6951196d4017e65e1cf9bd593afc9ac07548bcd8
Neo Liu <liukebin@me.com>
2020-01-01 20:17:06 +0800

    Add comment for new exported function

--- reader/parser/filter.go
+++ reader/parser/filter.go
@@ -5,6 +5,7 @@ import (
 	"unicode"
 )
 
+// StripInvalidCharacter used to remove invalid characters from feed content
 func StripInvalidCharacter(data string) string {
 	return strings.Map(func(s rune) rune {
 		if unicode.IsPrint(s) {
