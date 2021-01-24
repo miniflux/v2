miniflux/v2

commit 1ed3816d6fb5882b590481aa7a9b34852bcec392

PostgreSQL index optimizations

database/migrations.go
@@ -496,4 +496,12 @@ var migrations = []func(tx *sql.Tx) error{
 		`)
 		return err
 	},
+	func(tx *sql.Tx) (err error) {
+		_, err = tx.Exec(`
+			CREATE INDEX entries_feed_url_idx ON entries(feed_id, url);
+			CREATE INDEX entries_user_feed_idx ON entries(user_id, status, feed_id);
+			CREATE INDEX entries_user_status_changed_idx ON entries(user_id, status, changed_at);
+		`)
+		return err
+	},
 }

commit 56c0447e32d9d68f6b77b1d20022b45111ccf5aa

Fix regression introduced by PR #973
The relation "entries_user_feed_idx" already exists

Makefile
@@ -109,11 +109,17 @@ lint:
 integration-test:
 	psql -U postgres -c 'drop database if exists miniflux_test;'
 	psql -U postgres -c 'create database miniflux_test;'
-	DATABASE_URL=$(DB_URL) go run main.go -migrate
-	DATABASE_URL=$(DB_URL) ADMIN_USERNAME=admin ADMIN_PASSWORD=test123 go run main.go -create-admin
 	go build -o miniflux-test main.go
-	DATABASE_URL=$(DB_URL) ./miniflux-test -debug >/tmp/miniflux.log 2>&1 & echo "$$!" > "/tmp/miniflux.pid"
-	while ! echo exit | nc localhost 8080; do sleep 1; done >/dev/null
+
+	DATABASE_URL=$(DB_URL) \
+	ADMIN_USERNAME=admin \
+	ADMIN_PASSWORD=test123 \
+	CREATE_ADMIN=1 \
+	RUN_MIGRATIONS=1 \
+	DEBUG=1 \
+	./miniflux-test >/tmp/miniflux.log 2>&1 & echo "$$!" > "/tmp/miniflux.pid"
+	
+	while ! nc -z localhost 8080; do sleep 1; done
 	go test -v -tags=integration -count=1 miniflux.app/tests
 
 clean-integration-test:

database/migrations.go
@@ -499,7 +499,7 @@ var migrations = []func(tx *sql.Tx) error{
 	func(tx *sql.Tx) (err error) {
 		_, err = tx.Exec(`
 			CREATE INDEX entries_feed_url_idx ON entries(feed_id, url);
-			CREATE INDEX entries_user_feed_idx ON entries(user_id, status, feed_id);
+			CREATE INDEX entries_user_status_feed_idx ON entries(user_id, status, feed_id);
 			CREATE INDEX entries_user_status_changed_idx ON entries(user_id, status, changed_at);
 		`)
 		return err