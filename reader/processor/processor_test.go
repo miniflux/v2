c3f871b49b7cae515ca43308262ea59a502f5e20
Ilya Mateyko <me@astrophena.name>
2021-01-27 12:50:34 +0000

    Use YouTube video duration as read time
    This feature works by scraping YouTube website.
    
    To enable it, set the FETCH_YOUTUBE_WATCH_TIME environment variable to
    1.
    
    Resolves #972.

--- reader/processor/processor_test.go
+++ reader/processor/processor_test.go
@@ -6,6 +6,7 @@ package processor // import "miniflux.app/reader/processor"
 
 import (
 	"testing"
+	"time"
 
 	"miniflux.app/model"
 )
@@ -44,6 +45,35 @@ func TestAllowEntries(t *testing.T) {
 		result := isAllowedEntry(tc.feed, tc.entry)
 		if tc.expected != result {
 			t.Errorf(`Unexpected result, got %v for entry %q`, result, tc.entry.Title)
+		}
+	}
+}
+
+func TestParseISO8601(t *testing.T) {
+	var scenarios = []struct {
+		duration string
+		expected time.Duration
+	}{
+		// Live streams and radio.
+		{"PT0M0S", 0},
+		// https://www.youtube.com/watch?v=HLrqNhgdiC0
+		{"PT6M20S", (6 * time.Minute) + (20 * time.Second)},
+		// https://www.youtube.com/watch?v=LZa5KKfqHtA
+		{"PT5M41S", (5 * time.Minute) + (41 * time.Second)},
+		// https://www.youtube.com/watch?v=yIxEEgEuhT4
+		{"PT51M52S", (51 * time.Minute) + (52 * time.Second)},
+		// https://www.youtube.com/watch?v=bpHf1XcoiFs
+		{"PT80M42S", (1 * time.Hour) + (20 * time.Minute) + (42 * time.Second)},
+	}
+
+	for _, tc := range scenarios {
+		result, err := parseISO8601(tc.duration)
+		if err != nil {
+			t.Errorf("Got an error when parsing %q: %v", tc.duration, err)
+		}
+
+		if tc.expected != result {
+			t.Errorf(`Unexpected result, got %v for duration %q`, result, tc.duration)
 		}
 	}
 }
