864dd9f2192828a9321de5aecac7609fa3da7b82
Frédéric Guillot <f@miniflux.net>
2021-02-06 14:33:28 -0800

    Allow images with data URLs
    Only URLs with a mime-type image/* are allowed

--- http/response/builder.go
+++ http/response/builder.go
@@ -96,7 +96,7 @@ func (b *Builder) writeHeaders() {
 	b.headers["X-XSS-Protection"] = "1; mode=block"
 	b.headers["X-Content-Type-Options"] = "nosniff"
 	b.headers["X-Frame-Options"] = "DENY"
-	b.headers["Content-Security-Policy"] = "default-src 'self'; img-src *; media-src *; frame-src *"
+	b.headers["Content-Security-Policy"] = "default-src 'self'; img-src * data:; media-src *; frame-src *"
 
 	for key, value := range b.headers {
 		b.w.Header().Set(key, value)
