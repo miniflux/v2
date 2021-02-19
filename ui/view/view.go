4855fbd13f8c9be1a593db2db05052cb49954e0b
Frédéric Guillot <f@miniflux.net>
2021-02-18 20:34:58 -0800

    Use embed package for Javascript bundles instead of generated files

--- ui/view/view.go
+++ ui/view/view.go
@@ -41,7 +41,7 @@ func New(tpl *template.Engine, r *http.Request, sess *session.Session) *View {
 	b.params["flashErrorMessage"] = sess.FlashErrorMessage(request.FlashErrorMessage(r))
 	b.params["theme"] = theme
 	b.params["theme_checksum"] = static.StylesheetBundleChecksums[theme]
-	b.params["app_js_checksum"] = static.JavascriptsChecksums["app"]
-	b.params["sw_js_checksum"] = static.JavascriptsChecksums["service-worker"]
+	b.params["app_js_checksum"] = static.JavascriptBundleChecksums["app"]
+	b.params["sw_js_checksum"] = static.JavascriptBundleChecksums["service-worker"]
 	return b
 }
