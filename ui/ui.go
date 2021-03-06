be2d6af42738c8d696ea7e1668d9ee9a7d5f7b63
Alexandros Kosiaris <akosiaris@gmail.com>
2021-03-06 22:21:28 +0200

    Redirect to the view the request came from when marking feed as read
    Piggyback on the javascript used for POSTing to a url to obtain a more
    consistent behavior with the other options in the feed list (e.g. remove
    feed). That way, when a feed is marked as read, regardless which view
    the request originated from (feeds list view vs category feeds list
    view), we are redirected to the view we started from.
    
    While at it, add a confirmation dialog to mark feed as read. It was
    suggested in PR #1055 for the single feed option, but it probably makes
    sense if it was in the feeds list view too.

--- ui/ui.go
+++ ui/ui.go
@@ -80,7 +80,7 @@ func Serve(router *mux.Router, store *storage.Storage, pool *worker.Pool) {
 	uiRouter.HandleFunc("/feed/{feedID}/entries/all", handler.showFeedEntriesAllPage).Name("feedEntriesAll").Methods(http.MethodGet)
 	uiRouter.HandleFunc("/feed/{feedID}/entry/{entryID}", handler.showFeedEntryPage).Name("feedEntry").Methods(http.MethodGet)
 	uiRouter.HandleFunc("/feed/icon/{iconID}", handler.showIcon).Name("icon").Methods(http.MethodGet)
-	uiRouter.HandleFunc("/feed/{feedID}/mark-all-as-read", handler.markFeedAsRead).Name("markFeedAsRead").Methods(http.MethodGet)
+	uiRouter.HandleFunc("/feed/{feedID}/mark-all-as-read", handler.markFeedAsRead).Name("markFeedAsRead").Methods(http.MethodPost)
 
 	// Category pages.
 	uiRouter.HandleFunc("/category/{categoryID}/entry/{entryID}", handler.showCategoryEntryPage).Name("categoryEntry").Methods(http.MethodGet)
