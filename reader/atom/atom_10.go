5877048749520ca415df855160edde9213e806db
Frédéric Guillot <f@miniflux.net>
2021-03-20 20:38:29 -0700

    Improve handling of Atom text content with CDATA

--- reader/atom/atom_10.go
+++ reader/atom/atom_10.go
@@ -231,10 +231,12 @@ func (a *atom10Text) String() string {
 	var content string
 
 	switch {
-	case strings.HasPrefix(a.InnerXML, `<![CDATA[`):
-		content = a.CharData
 	case a.Type == "", a.Type == "text", a.Type == "text/plain":
-		content = a.InnerXML
+		if strings.HasPrefix(a.InnerXML, `<![CDATA[`) {
+			content = html.EscapeString(a.CharData)
+		} else {
+			content = a.InnerXML
+		}
 	case a.Type == "xhtml":
 		if a.XHTMLRootElement.InnerXML != "" {
 			content = a.XHTMLRootElement.InnerXML
