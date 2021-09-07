6b0fe414ac0d08ac6fcce0c3882c914df14266b4
oott123 <git@public.oott123.com>
2021-09-07 20:43:20 +0800

    refactor: prefix error messages

--- integration/telegrambot/telegrambot.go
+++ integration/telegrambot/telegrambot.go
@@ -14,19 +14,19 @@ import (
 func PushEntry(entry *model.Entry, integration *model.Integration) error {
 	bot, err := tgbotapi.NewBotAPI(integration.TelegramBotToken)
 	if err != nil {
-		return fmt.Errorf("create bot failed: %w", err)
+		return fmt.Errorf("telegrambot: create bot failed: %w", err)
 	}
 
 	t, err := template.New("message").Parse("{{ .Title }}\n<a href=\"{{ .URL }}\">{{ .URL }}</a>")
 	if err != nil {
-		return fmt.Errorf("parse template failed: %w", err)
+		return fmt.Errorf("telegrambot: parse template failed: %w", err)
 	}
 
 	var result bytes.Buffer
 
 	err = t.Execute(&result, entry)
 	if err != nil {
-		return fmt.Errorf("execute template failed: %w", err)
+		return fmt.Errorf("telegrambot: execute template failed: %w", err)
 	}
 
 	chatId, _ := strconv.ParseInt(integration.TelegramBotChatID, 10, 64)
@@ -34,7 +34,7 @@ func PushEntry(entry *model.Entry, integration *model.Integration) error {
 	msg.ParseMode = tgbotapi.ModeHTML
 	msg.DisableWebPagePreview = false
 	if _, err := bot.Send(msg); err != nil {
-		return fmt.Errorf("send message failed: %w", err)
+		return fmt.Errorf("telegrambot: send message failed: %w", err)
 	}
 
 	return nil
