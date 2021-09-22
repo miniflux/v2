cf1939f0631e87efa2025574ede065da3d2db2f6
NobeKanai <qidada4880@gmail.com>
2021-09-22 14:21:01 +0800

    Fix inconsistent navigation

--- ui/entry_unread.go
+++ ui/entry_unread.go
@@ -48,7 +48,7 @@ func (h *handler) showUnreadEntryPage(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	entryPaginationBuilder := storage.NewEntryPaginationBuilder(h.store, user.ID, entry.ID, user.EntryDirection)
+	entryPaginationBuilder := storage.NewEntryPaginationBuilder(h.store, user.ID, entry.ID, user.EntryOrder, user.EntryDirection)
 	entryPaginationBuilder.WithStatus(model.EntryStatusUnread)
 	entryPaginationBuilder.WithGloballyVisible()
 	prevEntry, nextEntry, err := entryPaginationBuilder.Entries()
