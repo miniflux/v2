1f60db03e36390000db5c9b934fbe9ea053b42a3
Theo Sun <theo.sun@outlook.com>
2021-12-18 22:21:11 +0800

    fix: lint

--- storage/entry.go
+++ storage/entry.go
@@ -105,7 +105,6 @@ func (s *Storage) UpdateEntryContent(entry *model.Entry) error {
 
 // createEntry add a new entry.
 func (s *Storage) createEntry(tx *sql.Tx, entry *model.Entry) error {
-
 	// when create a new entry, log the content keyword
 	s.LogKeywordForContent(entry.Content)
 
