40f958c5ef62029fc8c013bd1d87a443ccfe48b1
austinsuyoyo <avui67224307@gmail.com>
2022-03-25 01:34:37 +0800

    Add Traditional Chinese translation

--- locale/locale.go
+++ locale/locale.go
@@ -14,6 +14,7 @@ func AvailableLanguages() map[string]string {
 		"pl_PL": "Polski",
 		"pt_BR": "Português Brasileiro",
 		"zh_CN": "简体中文",
+		"zh_TW": "繁體中文",
 		"nl_NL": "Nederlands",
 		"ru_RU": "Русский",
 		"it_IT": "Italiano",
