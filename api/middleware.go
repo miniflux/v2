70c58bd346867bf462de3ce40a9d7ce26a1804be
Tomas Peterka <tomas@peterka.me>
2022-09-19 12:14:28 +0200

    API: Allow Basic authorization header

--- api/middleware.go
+++ api/middleware.go
@@ -25,7 +25,7 @@ func (m *middleware) handleCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "X-Auth-Token")
+		w.Header().Set("Access-Control-Allow-Headers", "X-Auth-Token, Authorization")
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Max-Age", "3600")
 			w.WriteHeader(http.StatusOK)
