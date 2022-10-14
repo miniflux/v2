9e521636e8e00c0d48c3866bbb9b2a794ff31d87
jgbresson <justingbresson@gmail.com>
2022-10-14 18:19:12 -0400

    Add theverge.com
    Add setting: add_dynamic_image

--- reader/rewrite/rules.go
+++ reader/rewrite/rules.go
@@ -29,6 +29,7 @@ var predefinedRules = map[string]string{
 	"quantamagazine.org":     `add_youtube_video_from_id, remove("h6:not(.byline,.post__title__kicker), #comments, .next-post__content, .footer__section, figure .outer--content, script")`,
 	"sentfromthemoon.com":    "add_image_title",
 	"thedoghousediaries.com": "add_image_title",
+	"theverge.com":           "add_dynamic_image",
 	"treelobsters.com":       "add_image_title",
 	"www.qwantz.com":         "add_image_title,add_mailto_subject",
 	"xkcd.com":               "add_image_title",
