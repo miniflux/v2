33c4b5188c1ae24d8fdea4261728bef1768fb912
Romain de Laage <romain.delaage@rdelaage.ovh>
2023-04-08 11:02:36 +0200

    Add a rewrite rule to remove clickbait titles

--- reader/processor/processor.go
+++ reader/processor/processor.go
@@ -85,7 +85,7 @@ func ProcessFeedEntries(store *storage.Storage, feed *model.Feed, user *model.Us
 			}
 		}
 
-		entry.Content = rewrite.Rewriter(url, entry.Content, feed.RewriteRules)
+		rewrite.Rewriter(url, entry, feed.RewriteRules)
 
 		// The sanitizer should always run at the end of the process to make sure unsafe HTML is filtered.
 		entry.Content = sanitizer.Sanitize(url, entry.Content)
@@ -168,13 +168,13 @@ func ProcessEntryWebPage(feed *model.Feed, entry *model.Entry, user *model.User)
 		return scraperErr
 	}
 
-	content = rewrite.Rewriter(url, content, entry.Feed.RewriteRules)
-	content = sanitizer.Sanitize(url, content)
-
 	if content != "" {
 		entry.Content = content
 		entry.ReadingTime = calculateReadingTime(content, user)
 	}
+
+	rewrite.Rewriter(url, entry, entry.Feed.RewriteRules)
+	entry.Content = sanitizer.Sanitize(url, entry.Content)
 
 	return nil
 }
