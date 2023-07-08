a756ef49b68ebc6d4147b2216d1920994d516d1d
Jean Khawand <22157081+jeankhawand@users.noreply.github.com>
2023-07-08 22:44:39 +0000

    😃fix conflicts

--- storage/integration.go
+++ storage/integration.go
@@ -284,14 +284,10 @@ func (s *Storage) UpdateIntegration(integration *model.Integration) error {
 			matrix_bot_chat_id=$43,
 			notion_enabled=$45,
 			notion_token=$46,
-<<<<<<< HEAD
 			notion_page_id=$47,
 			apprise_enabled=$48,
 			apprise_url=$49,
 			apprise_services_url=$50
-=======
-			notion_page_id=$47
->>>>>>> origin
 		WHERE
 			user_id=$44
 	`
@@ -343,12 +339,9 @@ func (s *Storage) UpdateIntegration(integration *model.Integration) error {
 			integration.NotionEnabled,
 			integration.NotionToken,
 			integration.NotionPageID,
-<<<<<<< HEAD
 			integration.AppriseEnabled,
 			integration.AppriseURL,
 			integration.AppriseServicesURL,
-=======
->>>>>>> origin
 			integration.UserID,
 		)
 	} else {
