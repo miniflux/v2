64c4c6b34743761d0f6ea02fc4d37594781afbf4
Yury Paraschenko <vvampirius@gmail.com>
2023-08-31 21:38:09 +0300

    Fix feed hide_globally property to use it with third-party clients.

--- internal/storage/entry_query_builder.go
+++ internal/storage/entry_query_builder.go
@@ -266,6 +266,7 @@ func (e *EntryQueryBuilder) GetEntries() (model.Entries, error) {
 			f.crawler,
 			f.user_agent,
 			f.cookie,
+			f.hide_globally,
 			f.no_media_player,
 			fi.icon_id,
 			u.timezone
@@ -331,6 +332,7 @@ func (e *EntryQueryBuilder) GetEntries() (model.Entries, error) {
 			&entry.Feed.Crawler,
 			&entry.Feed.UserAgent,
 			&entry.Feed.Cookie,
+			&entry.Feed.HideGlobally,
 			&entry.Feed.NoMediaPlayer,
 			&iconID,
 			&tz,
