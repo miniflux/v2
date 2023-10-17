cc44d14722612d6a3db4360a971ac7cd0844b7ed
Frédéric Guillot <f@miniflux.net>
2023-10-16 21:20:58 -0700

    Avoid excessive manual polling with default scheduler

--- internal/ui/category_refresh.go
+++ internal/ui/category_refresh.go
@@ -4,6 +4,7 @@
 package ui // import "miniflux.app/v2/internal/ui"
 
 import (
+	"log/slog"
 	"net/http"
 
 	"miniflux.app/v2/internal/http/request"
@@ -31,9 +32,14 @@ func (h *handler) refreshCategory(w http.ResponseWriter, r *http.Request) int64 
 		return 0
 	}
 
-	go func() {
-		h.pool.Push(jobs)
-	}()
+	slog.Info(
+		"Triggered a manual refresh of all feeds for a given category from the web ui",
+		slog.Int64("user_id", userID),
+		slog.Int64("category_id", categoryID),
+		slog.Int("nb_jobs", len(jobs)),
+	)
+
+	go h.pool.Push(jobs)
 
 	return categoryID
 }
