93c63ce83662bd5458860bc94e97e8e1953ee322
Ryan Stafford <ryan@nanorex.net>
2023-10-21 22:16:21 -0400

    use std http client

--- internal/integration/rssbridge/rssbridge.go
+++ internal/integration/rssbridge/rssbridge.go
@@ -1,17 +1,18 @@
 package rssbridge // import "miniflux.app/integration/rssbridge"
 
 import (
+	"bytes"
+	"net/http"
 	"net/url"
 	"regexp"
 
 	"miniflux.app/v2/internal/errors"
-	"miniflux.app/v2/internal/http/client"
 )
 
 func DetectBridge(rssbridgeURL, websiteURL string) (string, *errors.LocalizedError) {
 	u, err := url.Parse(rssbridgeURL)
 	if err != nil {
-		return "", errors.NewLocalizedError("rss-bridge: invalid url", err)
+		return "", errors.NewLocalizedError("RSS-Bridge: invalid url", err)
 	}
 	values := u.Query()
 	values.Add("action", "detect")
@@ -19,19 +20,19 @@ func DetectBridge(rssbridgeURL, websiteURL string) (string, *errors.LocalizedErr
 	values.Add("url", websiteURL)
 	u.RawQuery = values.Encode()
 
-	clt := client.New(u.String())
-
-	response, err := clt.Get()
+	response, err := http.Get(u.String())
 	if err != nil {
 		return "", errors.NewLocalizedError("RSS-Bridge: %v", err)
 	}
-	body := response.BodyAsString()
-	if response.HasServerFailure() {
+	defer response.Body.Close()
+	body := new(bytes.Buffer)
+	body.ReadFrom(response.Body)
+	if response.StatusCode >= 400 {
 		r, _ := regexp.Compile("<strong>Message:</strong>(.+)</div>")
-		if matches := r.FindStringSubmatch(body); len(matches) > 1 {
+		if matches := r.FindStringSubmatch(body.String()); len(matches) > 1 {
 			return "", errors.NewLocalizedError("RSS-Bridge: %v", matches[1])
 		}
-		return "", errors.NewLocalizedError("RSS-Bridge: Server Failure")
+		return "", errors.NewLocalizedError("RSS-Bridge: Server Failure (%d)", response.StatusCode)
 	}
-	return body, nil
+	return body.String(), nil
 }
