fbf9aaaa0322a7ed36fc40ff1ddc419a78ecf365
javex <florian.ruechel@inexplicity.de>
2023-11-05 18:24:52 +1030

    Add license header

--- internal/model/webauthn.go
+++ internal/model/webauthn.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright The Miniflux Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package model // import "miniflux.app/v2/internal/model"
 
 import (
--- internal/storage/webauthn.go
+++ internal/storage/webauthn.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright The Miniflux Authors. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package storage // import "miniflux.app/v2/internal/storage"
 
 import (
