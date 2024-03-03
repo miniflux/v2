d55b41080062915e728b6afb75f5623927f100f7
jvoisin <julien.voisin@dustri.org>
2024-03-04 00:08:55 +0100

    Use constant-time comparison for anti-csrf tokens
    This is probably completely overkill, but since anti-csrf tokens are secrets,
    they should be compared against untrusted inputs in constant time.

--- internal/crypto/crypto.go
+++ internal/crypto/crypto.go
@@ -7,6 +7,7 @@ import (
 	"crypto/hmac"
 	"crypto/rand"
 	"crypto/sha256"
+	"crypto/subtle"
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
@@ -59,4 +60,8 @@ func GenerateSHA256Hmac(secret string, data []byte) string {
 func GenerateUUID() string {
 	b := GenerateRandomBytes(16)
 	return fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+}
+
+func ConstantTimeCmp(a, b string) bool {
+	return subtle.ConstantTimeCompare([]byte(a), []byte(b)) == 1
 }
