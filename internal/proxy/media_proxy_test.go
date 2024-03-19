miniflux/v2

commit 66b8483791e58030c0cc3b5c5d82b0b0d5e87cec

Minor simplification of internal/proxy/proxy.go
- re-use ProxifiedUrl to implement AbsoluteProxifyURL, reducing the copy-pasta
- reduce the internal indentation of ProxifiedUrl by inverting some conditions

internal/proxy/proxy.go
@@ -19,51 +19,34 @@ import (
 
 // ProxifyURL generates a relative URL for a proxified resource.
 func ProxifyURL(router *mux.Router, link string) string {
-	if link != "" {
-		proxyImageUrl := config.Opts.ProxyUrl()
+	if link == "" {
+		return ""
+	}
 
-		if proxyImageUrl == "" {
-			mac := hmac.New(sha256.New, config.Opts.ProxyPrivateKey())
-			mac.Write([]byte(link))
-			digest := mac.Sum(nil)
-			return route.Path(router, "proxy", "encodedDigest", base64.URLEncoding.EncodeToString(digest), "encodedURL", base64.URLEncoding.EncodeToString([]byte(link)))
-		}
-
+	if proxyImageUrl := config.Opts.ProxyUrl(); proxyImageUrl != "" {
 		proxyUrl, err := url.Parse(proxyImageUrl)
 		if err != nil {
 			return ""
 		}
-
 		proxyUrl.Path = path.Join(proxyUrl.Path, base64.URLEncoding.EncodeToString([]byte(link)))
 		return proxyUrl.String()
 	}
-	return ""
+
+	mac := hmac.New(sha256.New, config.Opts.ProxyPrivateKey())
+	mac.Write([]byte(link))
+	digest := mac.Sum(nil)
+	return route.Path(router, "proxy", "encodedDigest", base64.URLEncoding.EncodeToString(digest), "encodedURL", base64.URLEncoding.EncodeToString([]byte(link)))
 }
 
 // AbsoluteProxifyURL generates an absolute URL for a proxified resource.
 func AbsoluteProxifyURL(router *mux.Router, host, link string) string {
-	if link != "" {
-		proxyImageUrl := config.Opts.ProxyUrl()
+	proxifiedUrl := ProxifyURL(router, link)
 
-		if proxyImageUrl == "" {
-			mac := hmac.New(sha256.New, config.Opts.ProxyPrivateKey())
-			mac.Write([]byte(link))
-			digest := mac.Sum(nil)
-			path := route.Path(router, "proxy", "encodedDigest", base64.URLEncoding.EncodeToString(digest), "encodedURL", base64.URLEncoding.EncodeToString([]byte(link)))
-			if config.Opts.HTTPS {
-				return "https://" + host + path
-			} else {
-				return "http://" + host + path
-			}
-		}
-
-		proxyUrl, err := url.Parse(proxyImageUrl)
-		if err != nil {
-			return ""
-		}
-
-		proxyUrl.Path = path.Join(proxyUrl.Path, base64.URLEncoding.EncodeToString([]byte(link)))
-		return proxyUrl.String()
+	if config.Opts.ProxyUrl() == "" {
+		return proxifiedUrl
 	}
-	return ""
+	if config.Opts.HTTPS {
+		return "https://" + host + proxifiedUrl
+	}
+	return "http://" + host + proxifiedUrl
 }

commit d4552a8bdca667059554ca64be8b12f63d4cb510

Fix regression in AbsoluteProxifyURL()
Regression introduced in commit 66b8483791e58030c0cc3b5c5d82b0b0d5e87cec

PR #2499

internal/googlereader/handler.go
@@ -766,7 +766,7 @@ func subscribe(newFeed Stream, category Stream, title string, store *storage.Sto
 	}
 
 	created, localizedError := mff.CreateFeed(store, userID, &feedRequest)
-	if err != nil {
+	if localizedError != nil {
 		return nil, localizedError.Error()
 	}
 

internal/proxy/proxy.go
@@ -7,6 +7,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
+	"log/slog"
 	"net/url"
 	"path"
 
@@ -18,35 +19,54 @@ import (
 )
 
 // ProxifyURL generates a relative URL for a proxified resource.
-func ProxifyURL(router *mux.Router, link string) string {
-	if link == "" {
+func ProxifyURL(router *mux.Router, mediaURL string) string {
+	if mediaURL == "" {
 		return ""
 	}
 
-	if proxyImageUrl := config.Opts.ProxyUrl(); proxyImageUrl != "" {
-		proxyUrl, err := url.Parse(proxyImageUrl)
-		if err != nil {
-			return ""
-		}
-		proxyUrl.Path = path.Join(proxyUrl.Path, base64.URLEncoding.EncodeToString([]byte(link)))
-		return proxyUrl.String()
+	if customProxyURL := config.Opts.ProxyUrl(); customProxyURL != "" {
+		return ProxifyURLWithCustomProxy(mediaURL, customProxyURL)
 	}
 
 	mac := hmac.New(sha256.New, config.Opts.ProxyPrivateKey())
-	mac.Write([]byte(link))
+	mac.Write([]byte(mediaURL))
 	digest := mac.Sum(nil)
-	return route.Path(router, "proxy", "encodedDigest", base64.URLEncoding.EncodeToString(digest), "encodedURL", base64.URLEncoding.EncodeToString([]byte(link)))
+	return route.Path(router, "proxy", "encodedDigest", base64.URLEncoding.EncodeToString(digest), "encodedURL", base64.URLEncoding.EncodeToString([]byte(mediaURL)))
 }
 
 // AbsoluteProxifyURL generates an absolute URL for a proxified resource.
-func AbsoluteProxifyURL(router *mux.Router, host, link string) string {
-	proxifiedUrl := ProxifyURL(router, link)
+func AbsoluteProxifyURL(router *mux.Router, host, mediaURL string) string {
+	if mediaURL == "" {
+		return ""
+	}
 
-	if config.Opts.ProxyUrl() == "" {
-		return proxifiedUrl
+	if customProxyURL := config.Opts.ProxyUrl(); customProxyURL != "" {
+		return ProxifyURLWithCustomProxy(mediaURL, customProxyURL)
 	}
+
+	proxifiedUrl := ProxifyURL(router, mediaURL)
+	scheme := "http"
 	if config.Opts.HTTPS {
-		return "https://" + host + proxifiedUrl
+		scheme = "https"
 	}
-	return "http://" + host + proxifiedUrl
+
+	return scheme + "://" + host + proxifiedUrl
+}
+
+func ProxifyURLWithCustomProxy(mediaURL, customProxyURL string) string {
+	if customProxyURL == "" {
+		return mediaURL
+	}
+
+	proxyUrl, err := url.Parse(customProxyURL)
+	if err != nil {
+		slog.Error("Incorrect custom media proxy URL",
+			slog.String("custom_proxy_url", customProxyURL),
+			slog.Any("error", err),
+		)
+		return mediaURL
+	}
+
+	proxyUrl.Path = path.Join(proxyUrl.Path, base64.URLEncoding.EncodeToString([]byte(mediaURL)))
+	return proxyUrl.String()
 }

internal/proxy/media_proxy_test.go
@@ -157,7 +157,82 @@ func TestProxyFilterWithHttpsAlways(t *testing.T) {
 	}
 }
 
+func TestAbsoluteProxyFilterWithHttpsAlways(t *testing.T) {
+	os.Clearenv()
+	os.Setenv("PROXY_OPTION", "all")
+	os.Setenv("PROXY_MEDIA_TYPES", "image")
+	os.Setenv("PROXY_PRIVATE_KEY", "test")
+
+	var err error
+	parser := config.NewParser()
+	config.Opts, err = parser.ParseEnvironmentVariables()
+	if err != nil {
+		t.Fatalf(`Parsing failure: %v`, err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/proxy/{encodedDigest}/{encodedURL}", func(w http.ResponseWriter, r *http.Request) {}).Name("proxy")
+
+	input := `<p><img src="https://website/folder/image.png" alt="Test"/></p>`
+	output := AbsoluteProxyRewriter(r, "localhost", input)
+	expected := `<p><img src="http://localhost/proxy/LdPNR1GBDigeeNp2ArUQRyZsVqT_PWLfHGjYFrrWWIY=/aHR0cHM6Ly93ZWJzaXRlL2ZvbGRlci9pbWFnZS5wbmc=" alt="Test"/></p>`
+
+	if expected != output {
+		t.Errorf(`Not expected output: got "%s" instead of "%s"`, output, expected)
+	}
+}
+
 func TestProxyFilterWithHttpsAlwaysAndCustomProxyServer(t *testing.T) {
+	os.Clearenv()
+	os.Setenv("PROXY_OPTION", "all")
+	os.Setenv("PROXY_MEDIA_TYPES", "image")
+	os.Setenv("PROXY_URL", "https://proxy-example/proxy")
+
+	var err error
+	parser := config.NewParser()
+	config.Opts, err = parser.ParseEnvironmentVariables()
+	if err != nil {
+		t.Fatalf(`Parsing failure: %v`, err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/proxy/{encodedDigest}/{encodedURL}", func(w http.ResponseWriter, r *http.Request) {}).Name("proxy")
+
+	input := `<p><img src="https://website/folder/image.png" alt="Test"/></p>`
+	output := ProxyRewriter(r, input)
+	expected := `<p><img src="https://proxy-example/proxy/aHR0cHM6Ly93ZWJzaXRlL2ZvbGRlci9pbWFnZS5wbmc=" alt="Test"/></p>`
+
+	if expected != output {
+		t.Errorf(`Not expected output: got "%s" instead of "%s"`, output, expected)
+	}
+}
+
+func TestProxyFilterWithHttpsAlwaysAndIncorrectCustomProxyServer(t *testing.T) {
+	os.Clearenv()
+	os.Setenv("PROXY_OPTION", "all")
+	os.Setenv("PROXY_MEDIA_TYPES", "image")
+	os.Setenv("PROXY_URL", "http://:8080example.com")
+
+	var err error
+	parser := config.NewParser()
+	config.Opts, err = parser.ParseEnvironmentVariables()
+	if err != nil {
+		t.Fatalf(`Parsing failure: %v`, err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/proxy/{encodedDigest}/{encodedURL}", func(w http.ResponseWriter, r *http.Request) {}).Name("proxy")
+
+	input := `<p><img src="https://website/folder/image.png" alt="Test"/></p>`
+	output := ProxyRewriter(r, input)
+	expected := `<p><img src="https://website/folder/image.png" alt="Test"/></p>`
+
+	if expected != output {
+		t.Errorf(`Not expected output: got "%s" instead of "%s"`, output, expected)
+	}
+}
+
+func TestAbsoluteProxyFilterWithHttpsAlwaysAndCustomProxyServer(t *testing.T) {
 	os.Clearenv()
 	os.Setenv("PROXY_OPTION", "all")
 	os.Setenv("PROXY_MEDIA_TYPES", "image")