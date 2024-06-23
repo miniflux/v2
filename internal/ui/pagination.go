c75cf556a6dcf50245da07b9f118a2b31bdc353d
Wojciech Kapcia <wojtek@tigase.org>
2024-06-23 13:22:49 +0200

    Add navigation to Last/First page  #1629

--- internal/ui/pagination.go
+++ internal/ui/pagination.go
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package ui // import "miniflux.app/v2/internal/ui"
+import (
+	"math"
+)
 
 type pagination struct {
 	Route        string
@@ -9,17 +12,27 @@ type pagination struct {
 	Offset       int
 	ItemsPerPage int
 	ShowNext     bool
+	ShowLast     bool
+	ShowFirst    bool
 	ShowPrev     bool
 	NextOffset   int
+	LastOffset   int
 	PrevOffset   int
+	FirstOffset  int
 	SearchQuery  string
 }
 
 func getPagination(route string, total, offset, nbItemsPerPage int) pagination {
 	nextOffset := 0
 	prevOffset := 0
+
+	firstOffset := 0
+	lastOffset := (int)(math.Floor(float64(total/nbItemsPerPage))) * nbItemsPerPage
+
 	showNext := (total - offset) > nbItemsPerPage
 	showPrev := offset > 0
+	showLast := showNext
+	showFirst := showPrev
 
 	if showNext {
 		nextOffset = offset + nbItemsPerPage
@@ -35,8 +48,12 @@ func getPagination(route string, total, offset, nbItemsPerPage int) pagination {
 		Offset:       offset,
 		ItemsPerPage: nbItemsPerPage,
 		ShowNext:     showNext,
+		ShowLast:     showLast,
 		NextOffset:   nextOffset,
+		LastOffset:   lastOffset,
 		ShowPrev:     showPrev,
+		ShowFirst:    showFirst,
 		PrevOffset:   prevOffset,
+		FirstOffset:  firstOffset,
 	}
 }
