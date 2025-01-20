4418c3102cdc93839f6ba2696784644e5aed58fd
jvoisin <julien.voisin@dustri.org>
2025-01-20 16:14:22 +0100

    Remove superfluous parenthesis

--- internal/locale/plural.go
+++ internal/locale/plural.go
@@ -5,7 +5,7 @@ package locale // import "miniflux.app/v2/internal/locale"
 
 // See https://localization-guide.readthedocs.io/en/latest/l10n/pluralforms.html
 // And http://www.unicode.org/cldr/charts/29/supplemental/language_plural_rules.html
-var pluralForms = map[string](func(n int) int){
+var pluralForms = map[string]func(n int) int{
 	// nplurals=2; plural=(n != 1);
 	"default": func(n int) int {
 		if n != 1 {
