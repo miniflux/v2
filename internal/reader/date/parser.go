1373754c5f7ebc82196179097fbc00af5b53df2c
jvoisin <julien.voisin@dustri.org>
2025-02-02 22:11:05 +0100

    Use an else-if instead of two if in internal/reader/date/parser.go

--- internal/reader/date/parser.go
+++ internal/reader/date/parser.go
@@ -354,9 +354,7 @@ func parseLocalTimeDates(layout, ds string) (t time.Time, err error) {
 	// Workaround for dates that don't use GMT.
 	if strings.HasSuffix(ds, "PST") || strings.HasSuffix(ds, "PDT") {
 		loc, _ = time.LoadLocation("America/Los_Angeles")
-	}
-
-	if strings.HasSuffix(ds, "EST") || strings.HasSuffix(ds, "EDT") {
+	} else if strings.HasSuffix(ds, "EST") || strings.HasSuffix(ds, "EDT") {
 		loc, _ = time.LoadLocation("America/New_York")
 	}
 
