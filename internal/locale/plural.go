c1af89301481805db872d1d6c3481cfa750f9c9e
krvpb024 <krvpb024@protonmail.com>
2025-02-13 23:31:58 +0800

    feat(locale): add Taiwanese POJ (nan-Latn-pehoeji) support

--- internal/locale/plural.go
+++ internal/locale/plural.go
@@ -88,6 +88,9 @@ var pluralForms = map[string]func(n int) int{
 	"zh_TW": func(n int) int {
 		return 0
 	},
+	"nan_Latn_pehoeji": func(n int) int {
+		return 0
+	},
 }
 
 // nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);
