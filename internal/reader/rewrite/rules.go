c00bd9206fe99f45d34f2620429d92fe5c99979f
AiraNadih <128119996+AiraNadih@users.noreply.github.com>
2025-03-02 20:16:00 +0000

    refactor(rewrite): reorganize referer rules and remove obsolete mapping

--- internal/reader/rewrite/rules.go
+++ internal/reader/rewrite/rules.go
@@ -50,31 +50,29 @@ func GetRefererForURL(u string) string {
 	}
 
 	switch parsedUrl.Hostname() {
-	case "moyu.im":
-		return "https://i.jandan.net"
-	case "i.pximg.net":
-		return "https://www.pixiv.net"
-	case "sp1.piokok.com":
-		return "https://sp1.piokok.com"
+	case "appinn.com":
+		return "https://appinn.com"
+	case "bjp.org.cn":
+		return "https://bjp.org.cn"
 	case "cdnfile.sspai.com":
 		return "https://sspai.com"
 	case "f.video.weibocdn.com":
 		return "https://weibo.com"
+	case "i.pximg.net":
+		return "https://www.pixiv.net"
 	case "img.hellogithub.com":
 		return "https://hellogithub.com"
-	case "bjp.org.cn":
-		return "https://bjp.org.cn"
-	case "appinn.com":
-		return "https://appinn.com"
+	case "moyu.im":
+		return "https://i.jandan.net"
 	}
 
 	switch {
-	case strings.HasSuffix(parsedUrl.Hostname(), ".sinaimg.cn"):
-		return "https://weibo.com"
 	case strings.HasSuffix(parsedUrl.Hostname(), ".cdninstagram.com"):
 		return "https://www.instagram.com"
 	case strings.HasSuffix(parsedUrl.Hostname(), ".moyu.im"):
 		return "https://i.jandan.net"
+	case strings.HasSuffix(parsedUrl.Hostname(), ".sinaimg.cn"):
+		return "https://weibo.com"
 	}
 
 	return ""
