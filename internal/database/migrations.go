52b184394f3173373bc6a4cb74e09282cdc778f4
Frédéric Guillot <f@miniflux.net>
2025-05-23 13:18:34 -0700

    fix(migrations): prevent failure at v45 with long entry URLs
    Fixes an issue where upgrading from older versions of Miniflux could fail with the following PostgreSQL error:
    
    ```
    [FATAL] [Migration v45] pq: index row size 2744 exceeds btree version 4 maximum 2704 for index "entries_feed_url_idx"
    ```

--- internal/database/migrations.go
+++ internal/database/migrations.go
@@ -509,7 +509,7 @@ var migrations = []func(tx *sql.Tx, driver string) error{
 	},
 	func(tx *sql.Tx, _ string) (err error) {
 		_, err = tx.Exec(`
-			CREATE INDEX entries_feed_url_idx ON entries(feed_id, url);
+			CREATE INDEX entries_feed_url_idx ON entries(feed_id, url) WHERE length(url) < 2000;
 			CREATE INDEX entries_user_status_feed_idx ON entries(user_id, status, feed_id);
 			CREATE INDEX entries_user_status_changed_idx ON entries(user_id, status, changed_at);
 		`)
