457af8086bee2e93dd51e3bf566bc9d68fd9b49a
jvoisin <julien.voisin@dustri.org>
2025-06-17 16:34:44 +0200

    perf(rewrite): make getPredefinedRewriteRules O(1)

--- internal/reader/rewrite/rules.go
+++ internal/reader/rewrite/rules.go
@@ -32,11 +32,11 @@ var predefinedRules = map[string]string{
 	"optipess.com":           "add_image_title",
 	"peebleslab.com":         "add_image_title",
 	"quantamagazine.org":     `add_youtube_video_from_id, remove("h6:not(.byline,.post__title__kicker), #comments, .next-post__content, .footer__section, figure .outer--content, script")`,
+	"qwantz.com":             "add_image_title,add_mailto_subject",
 	"sentfromthemoon.com":    "add_image_title",
 	"thedoghousediaries.com": "add_image_title",
 	"theverge.com":           `add_dynamic_image, remove("div.duet--recirculation--related-list, .hidden")`,
 	"treelobsters.com":       "add_image_title",
-	"www.qwantz.com":         "add_image_title,add_mailto_subject",
 	"xkcd.com":               "add_image_title",
 	"youtube.com":            "add_youtube_video",
 }
