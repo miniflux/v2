f31a784eaa364426a6486b027f5ea919dc787471
jvoisin <julien.voisin@dustri.org>
2025-06-18 22:29:11 +0200

    refactor(processor): refactor common code into a fetchWatchTime function
    Both nebula and odysee were using the same function to parse time.

--- internal/reader/processor/odysee.go
+++ internal/reader/processor/odysee.go
@@ -4,18 +4,9 @@
 package processor // import "miniflux.app/v2/internal/reader/processor"
 
 import (
-	"errors"
-	"fmt"
-	"log/slog"
-	"net/url"
-	"strconv"
-
-	"github.com/PuerkitoBio/goquery"
-
 	"miniflux.app/v2/internal/config"
 	"miniflux.app/v2/internal/model"
-	"miniflux.app/v2/internal/proxyrotator"
-	"miniflux.app/v2/internal/reader/fetcher"
+	"miniflux.app/v2/internal/urllib"
 )
 
 func shouldFetchOdyseeWatchTime(entry *model.Entry) bool {
@@ -23,42 +14,9 @@ func shouldFetchOdyseeWatchTime(entry *model.Entry) bool {
 		return false
 	}
 
-	u, err := url.Parse(entry.URL)
-	if err != nil {
-		return false
-	}
-
-	return u.Hostname() == "odysee.com"
+	return urllib.DomainWithoutWWW(entry.URL) == "odysee.com"
 }
 
 func fetchOdyseeWatchTime(websiteURL string) (int, error) {
-	requestBuilder := fetcher.NewRequestBuilder()
-	requestBuilder.WithTimeout(config.Opts.HTTPClientTimeout())
-	requestBuilder.WithProxyRotator(proxyrotator.ProxyRotatorInstance)
-
-	responseHandler := fetcher.NewResponseHandler(requestBuilder.ExecuteRequest(websiteURL))
-	defer responseHandler.Close()
-
-	if localizedError := responseHandler.LocalizedError(); localizedError != nil {
-		slog.Warn("Unable to fetch Odysee watch time", slog.String("website_url", websiteURL), slog.Any("error", localizedError.Error()))
-		return 0, localizedError.Error()
-	}
-
-	doc, docErr := goquery.NewDocumentFromReader(responseHandler.Body(config.Opts.HTTPClientMaxBodySize()))
-	if docErr != nil {
-		return 0, docErr
-	}
-
-	durs, exists := doc.FindMatcher(goquery.Single(`meta[property="og:video:duration"]`)).Attr("content")
-	// durs contains video watch time in seconds
-	if !exists {
-		return 0, errors.New("duration has not found")
-	}
-
-	dur, err := strconv.ParseInt(durs, 10, 64)
-	if err != nil {
-		return 0, fmt.Errorf("unable to parse duration %s: %v", durs, err)
-	}
-
-	return int(dur / 60), nil
+	return fetchWatchTime(websiteURL, `meta[property="og:video:duration"]`, false)
 }
