dfee274b4d6bc93a2f4e107682e9c379ce2df010
jvoisin <julien.voisin@dustri.org>
2025-08-02 22:58:36 +0200

    feat(integrations): automatically disable googlereader/fever when not used #3543
    When there is no user of Fever/GoogleReader, there is no need to expose their
    endpoints. This reduces quite a bit the exposition surface of miniflux, while
    not breaking any existing deployments, and is pretty self-contained.

--- internal/storage/integration.go
+++ internal/storage/integration.go
@@ -52,6 +52,21 @@ func (s *Storage) UserByFeverToken(token string) (*model.User, error) {
 	}
 }
 
+func (s *Storage) IsFeverUsed() (bool, error) {
+	query := `SELECT true FROM integrations WHERE fever_enabled=true LIMIT 1`
+	result := false
+	err := s.db.QueryRow(query).Scan(&result)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, fmt.Errorf(`store: unable to check if fever is used: %v`, err)
+	}
+
+	return result, nil
+}
+
 // GoogleReaderUserCheckPassword validates the Google Reader hashed password.
 func (s *Storage) GoogleReaderUserCheckPassword(username, password string) error {
 	var hash string
@@ -103,6 +118,21 @@ func (s *Storage) GoogleReaderUserGetIntegration(username string) (*model.Integr
 	}
 
 	return &integration, nil
+}
+
+func (s *Storage) IsGoogleReaderUsed() (bool, error) {
+	query := `SELECT true FROM integrations WHERE googlereader_enabled=true LIMIT 1`
+	var result bool
+	err := s.db.QueryRow(query).Scan(&result)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, fmt.Errorf(`store: unable to check if Google Reader is used: %v`, err)
+	}
+
+	return result, nil
 }
 
 // Integration returns user integration settings.
