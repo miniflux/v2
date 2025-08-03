a43d150a277c15676e6c5095e61fdc4968eff713
Julien Voisin <jvoisin@users.noreply.github.com>
2025-08-03 21:53:10 +0200

    refactor(parser): centralize seek logic and provide a hint for the compiler to eliminate a useless bound check
    - Move the seeking inside of DetectFeedFormat instead of having it everywhere
      in ParseFeed
    - Provide a hint for the compiler to eliminate a useless bound check in
      DetectJSONFormat, otherwise it'll check that buffer[i] is valid on every
      iteration of the loop. This shouldn't make a big difference, but oh well.

--- internal/reader/parser/parser.go
+++ internal/reader/parser/parser.go
@@ -18,20 +18,15 @@ var ErrFeedFormatNotDetected = errors.New("parser: unable to detect feed format"
 
 // ParseFeed analyzes the input data and returns a normalized feed object.
 func ParseFeed(baseURL string, r io.ReadSeeker) (*model.Feed, error) {
-	r.Seek(0, io.SeekStart)
 	format, version := DetectFeedFormat(r)
 	switch format {
 	case FormatAtom:
-		r.Seek(0, io.SeekStart)
 		return atom.Parse(baseURL, r, version)
 	case FormatRSS:
-		r.Seek(0, io.SeekStart)
 		return rss.Parse(baseURL, r)
 	case FormatJSON:
-		r.Seek(0, io.SeekStart)
 		return json.Parse(baseURL, r)
 	case FormatRDF:
-		r.Seek(0, io.SeekStart)
 		return rdf.Parse(baseURL, r)
 	default:
 		return nil, ErrFeedFormatNotDetected
