f0a53104f811e4cb36dea22b0d1fea94657047a5
jvoisin <julien.voisin@dustri.org>
2025-08-05 15:23:36 +0200

    perf(storage): speed up marking a feed as read
    There is no need to perform a heavy-weight SQL query gathering all the
    information available on a feed when we're only interested in its last check
    timestamp.

--- internal/ui/feed_mark_as_read.go
+++ internal/ui/feed_mark_as_read.go
@@ -15,19 +15,13 @@ func (h *handler) markFeedAsRead(w http.ResponseWriter, r *http.Request) {
 	feedID := request.RouteInt64Param(r, "feedID")
 	userID := request.UserID(r)
 
-	feed, err := h.store.FeedByID(userID, feedID)
-
+	checkedAt, err := h.store.CheckedAt(userID, feedID)
 	if err != nil {
-		html.ServerError(w, r, err)
-		return
-	}
-
-	if feed == nil {
 		html.NotFound(w, r)
 		return
 	}
 
-	if err = h.store.MarkFeedAsRead(userID, feedID, feed.CheckedAt); err != nil {
+	if err = h.store.MarkFeedAsRead(userID, feedID, checkedAt); err != nil {
 		html.ServerError(w, r, err)
 		return
 	}
