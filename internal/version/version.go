c1af510ead4437e1c2237618b8f071f8feb0847d
Frédéric Guillot <f@miniflux.net>
2025-08-16 11:54:15 -0700

    feat(version): use Golang's builtin vcs feature to get commit and build date

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,9 +3,36 @@
 
 package version // import "miniflux.app/v2/internal/version"
 
+import (
+	"runtime/debug"
+	"time"
+)
+
 // Variables populated at build time.
 var (
-	Version   = "dev"
-	Commit    = "HEAD"
-	BuildDate = "undefined"
+	Version   = "Development Version"
+	Commit    = getCommit()
+	BuildDate = getBuildDate()
 )
+
+func getCommit() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return setting.Value[:8] // Short commit hash
+			}
+		}
+	}
+	return "HEAD"
+}
+
+func getBuildDate() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.time" {
+				return setting.Value
+			}
+		}
+	}
+	return time.Now().Format(time.RFC3339)
+}
