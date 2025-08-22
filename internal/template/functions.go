487adc04f1b428ba6da239d9f62c6b6a1292b232
jvoisin <julien.voisin@dustri.org>
2025-08-22 14:05:10 +0200

    refactor(template): extract the CSP in a function and systematically use nonces.
    Having the CSP built in a function instead of in the template makes it easier
    to properly construct it. This was also the opportunity to switch from
    default-src 'self' to default-src 'none', to deny everything that isn't
    explicitly allowed, instead of allowing everything coming from 'self'.
    
    Moreover, as Miniflux is shoving the content of feeds in the same origin as
    itself, using self doesn't do much security-wise. It's much better to
    systematically use a nonce-based policy, so that an attacker able to bypass the
    sanitization will have to guess the nonce to gain arbitrary javascript
    execution.
    
    While the merge-request has been tested locally, it would still be prudent to
    thoroughly test it before merging, as it has the potential to break the
    user-interface should weird constructs be used.

--- internal/template/functions.go
+++ internal/template/functions.go
@@ -33,6 +33,7 @@ type funcMap struct {
 func (f *funcMap) Map() template.FuncMap {
 	return template.FuncMap{
 		"contains":         strings.Contains,
+		"csp":              csp,
 		"startsWith":       strings.HasPrefix,
 		"formatFileSize":   formatFileSize,
 		"dict":             dict,
@@ -114,6 +115,40 @@ func (f *funcMap) Map() template.FuncMap {
 			return ""
 		},
 	}
+}
+
+func csp(user *model.User, nonce string) string {
+	policies := map[string]string{
+		"default-src":               "'none'",
+		"frame-src":                 "*",
+		"img-src":                   "* data:",
+		"manifest-src":              "'self'",
+		"media-src":                 "*",
+		"require-trusted-types-for": "'script'",
+		"script-src":                "'nonce-" + nonce + "' 'strict-dynamic'",
+		"style-src":                 "'nonce-" + nonce + "'",
+		"trusted-types":             "html url",
+		"connect-src":               "'self'",
+	}
+
+	if user != nil {
+		if user.ExternalFontHosts != "" {
+			policies["font-src"] = user.ExternalFontHosts
+			if user.Stylesheet != "" {
+				policies["style-src"] += " " + user.ExternalFontHosts
+			}
+		}
+	}
+
+	var policy strings.Builder
+	for key, value := range policies {
+		policy.WriteString(key)
+		policy.WriteString(" ")
+		policy.WriteString(value)
+		policy.WriteString("; ")
+	}
+
+	return `<meta http-equiv="Content-Security-Policy" content="` + policy.String() + `">`
 }
 
 func dict(values ...any) (map[string]any, error) {
