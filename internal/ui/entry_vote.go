ebf15df57d2ce649ee0c882a394bbbebabc7eeae
JPFrancoia <jeanpatrick.francoia@gmail.com>
2025-12-16 17:33:45 +0000

    fix(vote): fix incorrect correction between integer types (adding validation)

--- internal/ui/entry_vote.go
+++ internal/ui/entry_vote.go
@@ -12,13 +12,15 @@ import (
 
 func (h *handler) updateEntryVote(w http.ResponseWriter, r *http.Request) {
 	entryID := request.RouteInt64Param(r, "entryID")
-	voteValue := int(request.RouteInt64Param(r, "vote"))
+	voteValue64 := request.RouteInt64Param(r, "vote")
 
 	// Validate vote value
-	if voteValue < -1 || voteValue > 1 {
+	if voteValue64 < -1 || voteValue64 > 1 {
 		json.BadRequest(w, r, nil)
 		return
 	}
+
+	voteValue := int(voteValue64)
 
 	if err := h.store.UpdateEntryVote(request.UserID(r), entryID, voteValue); err != nil {
 		json.ServerError(w, r, err)
