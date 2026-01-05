6c3fa090c8212ca6b0e32f2fbf1c1dd0fc91fc52
Frédéric Guillot <f@miniflux.net>
2026-01-05 15:40:03 -0800

    feat(googlereader)!: remove CORS handler
    The Google Reader API is not supposed to be used by web clients.
    
    Removing CORS should not break any Google Reader client and it
    reduces the attack surface.

--- internal/googlereader/middleware.go
+++ internal/googlereader/middleware.go
@@ -25,19 +25,6 @@ func newMiddleware(s *storage.Storage) *middleware {
 	return &middleware{s}
 }
 
-func (m *middleware) handleCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Authorization")
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func (m *middleware) apiKeyAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		clientIP := request.ClientIP(r)
