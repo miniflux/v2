223d29e5d956e744e9af916aea10fc3568dc0881
jvoisin <julien.voisin@dustri.org>
2026-02-17 00:57:01 +0100

    refactor(icon): remove two loops
    1. If the websiteURL is equal to the RootURL, there is no need to check them
       both.
    2. Group the icon-looking-queries into a single one.

--- internal/reader/icon/finder_test.go
+++ internal/reader/icon/finder_test.go
@@ -246,12 +246,10 @@ func TestFindIconURLsFromHTMLDocument_DataURLs(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// The function processes queries in order: rel="icon", then rel="shortcut icon", etc.
-	// So both rel="icon" links are found first, then the rel="shortcut icon" link
 	expected := []string{
 		"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChAGAhGAQ+QAAAABJRU5ErkJggg==",
-		"https://example.org/regular-icon.ico",
 		"data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg'></svg>",
+		"https://example.org/regular-icon.ico",
 	}
 
 	if len(iconURLs) != len(expected) {
