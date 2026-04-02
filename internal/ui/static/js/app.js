86abad8000dc55e8c510318eb46434603c369fe8
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2026-04-02 11:58:32 +0200

    feat(webpush): add a javascript snippet that register for webpush

--- internal/ui/static/js/app.js
+++ internal/ui/static/js/app.js
@@ -1092,7 +1092,16 @@ function initializeServiceWorker() {
                 console.error("Service Worker registration failed:", error);
             });
         }
-    }
+		navigator.serviceWorker.getRegistrations()
+			.then(function(registration) {
+				return registration[0].pushManager.getSubscription();
+			})
+			.then(function(subscription) {
+				if (!subscription) {
+					subscribeWebpush();
+				}
+			});
+	}
 
     // PWA installation prompt handling
     window.addEventListener("beforeinstallprompt", (event) => {
@@ -1265,6 +1274,41 @@ function initializeClickHandlers() {
             handleOriginalLink(event);
         }
     }, true);
+}
+
+function urlBase64ToUint8Array(base64String) {
+	const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+	const base64 = (base64String + padding)
+	.replace(/\-/g, '+')
+	.replace(/_/g, '/');
+	const rawData = window.atob(base64);
+	return Uint8Array.from([...rawData].map(char => char.charCodeAt(0)));
+}
+
+function subscribeWebpush() {
+	navigator.serviceWorker.getRegistrations()
+		.then(function(registration) {
+			return fetch('/vapid').then(response => response.text()).then(vapidPublicKey => {
+				return registration[0].pushManager.subscribe({
+					userVisibleOnly: true,
+					applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+				});
+			})
+		})
+		.then(function(subscription) {
+			// Send the subscription to the server
+			let authScheme = 'vapid';
+			const userAgent = navigator.userAgent;
+			if (userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Edg") === -1) {
+				authScheme = 'webpush';
+			}
+			sendPOSTRequest('/register-webpush', {
+				endpoint: subscription.endpoint,
+				key: subscription.options.applicationServerKey,
+				auth: authScheme,
+			})
+		})
+		.catch(err => console.error(err));
 }
 
 // Initialize application handlers
